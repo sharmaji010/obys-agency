e750934d3613c1ed6fee8eb6d40b6009895c5003
sharmaji010 <as686316@gmail.com>
2024-09-08 15:58:16 +0530

    locomotive done

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+function locomotiveAnimation(){
+    gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
 function loaderAnimation(){
     var tl = gsap.timeline()
 tl.from(".line h1",{
@@ -30,19 +62,23 @@ tl.to(".line h2",{
 
 tl.to("#loader",{
     opacity: 0,
-    delay:2.3,
-    duration: .4
+    y:-1200,
+    delay:0,
+    duration: 1
 })
 // 
 
-tl.from("#page1",{
-    y:1200,
-    delay:.2
-})
 
+// tl.from("#page1",{
+//     y:1200,
+//     duration:.5,
+//     delay:.2
+// })
 tl.to("#loader",{
     display: "none"
 })
+
+
 
 tl.from("#nav",{
     opacity:0
@@ -52,6 +88,9 @@ tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
     y:140,
     stagger:.1,
 })
+tl.from("#hero1, #page2",{
+    opacity: 0
+},"-=1.2")
 }
 
 function cursurAnimation(){
@@ -63,18 +102,12 @@ function cursurAnimation(){
         
     })
     Shery.makeMagnet("#nav-part2 h4", {
-
     });
 }
+// cursurAnimation()
 loaderAnimation()
-cursurAnimation()
+locomotiveAnimation()
 
-// document.querySelector("#nav-part2").addEventListener("mouseenter",function(){
-//    cursur.style.scale = 2
-// })
-// document.querySelector("#nav-part2").addEventListener("mouseleave",function(){
-//    cursur.style.scale = 1
-// })
 
 
 
